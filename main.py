c459b4914a6940ffc7cc861915b8bb8fa9dc50d8
Kyle Hughes <kylehughes1@gmail.com>
2018-06-15 14:27:59 -0500

    added json history saving/loading

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #/usr/bin/env python
 
 from hashlib import md5, sha1, sha256, sha384, sha512
+from json import dump, load
 from os import path
-from shutil import copyfile, copytree
+from shutil import copyfile
 from time import sleep
 
 """
@@ -14,11 +15,12 @@ targetFiles = (
         'testdata.txt',
     )
 
-tmpDir = '/tmp'
-fileHashes = dict()
+tmpDir, fieldnames = '/tmp', ('filename', 'checksum')
+fileHashes = [{'filename': f, 'checksum': 0} for f in targetFiles]
 
-for f in targetFiles:
-    fileHashes[f] = None
+# Write to disk every x number of detected changes to lessen wear on local storage
+writeThreshold = 50
+
 
 def make_temp_copy(currentFile):
     """Duplicate the file to a tmp volume to avoid reading while writing"""
@@ -39,16 +41,33 @@ def check_hash(currentFile, blocksize=512000):
     return hasher.hexdigest()
 
 
+def write_hashes():
+    with open('hashes.json', 'w') as hashes:
+        dump(fileHashes, hashes, sort_keys=True, indent=4)
+
+
+def read_hashes():
+    if path.exists('hashes.json'):
+        with open('hashes.json', 'r') as hashes:
+            fileHashes = load(hashes)
+
+
 def main():
+    read_hashes()
+    detected = 0
     while True:
         changed = []
-        for f in targetFiles:
-            duplicate = make_temp_copy(f)
+        for f in fileHashes:
+            duplicate = make_temp_copy(f['filename'])
             hashed = check_hash(duplicate)
-            if hashed != fileHashes[f]:
+            if hashed != f['checksum']:
                 changed.append(f)
-                fileHashes[f] = hashed
+                f['checksum'] = hashed
         if changed:
+            detected += 1
+            if detected >= writeThreshold or detected is 1:
+                print('Writing file hashes to disk')
+                write_hashes()
             print('Staged for upload:')
             for stagedFile in changed:
                 print(stagedFile)
